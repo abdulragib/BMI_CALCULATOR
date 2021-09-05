15ba0bc082ff0561d6c31c398b7c95bc4cc0780a
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-09-05 11:39:58 +0530

    all dart file arranged into directory

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,9 +1,11 @@
-import 'package:bmi_calculator/results_page.dart';
+import 'results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'reusuable_card.dart';
-import 'icon_content.dart';
-import 'constants.dart';
+import '/components/reusuable_card.dart';
+import '/components/icon_content.dart';
+import '/constants.dart';
+import '/components/bottom_button.dart';
+import '/components/round_icon_button.dart';
 
 enum Gender {
   male,
@@ -182,22 +184,12 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          GestureDetector(
+          BottomButton(
+            buttonTitle: 'CALCULATE',
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ResultsPage()),
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ResultsPage()));
             },
-            child: Container(
-              child: Center(
-                  child: Text("CALCULATE", style: kLargeButtonTextStyle)),
-              color: kBottomContainerColor,
-              margin: EdgeInsets.only(top: 10.0),
-              padding: EdgeInsets.only(bottom: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
-            ),
           ),
         ],
       ),
@@ -205,25 +197,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 0.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
 
 //both final and constant is immutable or unchangable.
