879d80684418bfa43d97095cfd21a65607dbb4d0
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-09-06 18:48:01 +0530

    calculator brain

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.height, this.weight});
+  final int height;
+  final int weight;
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'UnderWeight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal boby weight. Try to exercise more';
+    } else if (_bmi >= 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
+}
+
+//hieght/100 means we are coverting into meters bcz bydefault its value is in centimeters.
+//pow(height/100,2), its means that we are we are making square of height in meters.
